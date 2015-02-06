207ddd8eeb02d24a754bbc78a268039f4e82926f
Robert Tingirica <tingirica.robert@gmail.com>
2015-02-06 17:27:04 +0200

    Added check for url
    Adds function to check if the url is github;
    Adds warnings if fields are empty;
    Adds functionality for getting percentage of github code;

--- static/rouletteApp.js
+++ static/rouletteApp.js
@@ -8,19 +8,78 @@ app.config(['$interpolateProvider', function ($interpolateProvider) {
 
 app.controller('firstController', ['$scope', '$http', function ($scope, $http) {
     $scope.visible = false;
+    $scope.showWarning = false;
+    $scope.showGithubWarning = false;
+
+    $scope.getLocation = function(href) {
+        var l = document.createElement("a");
+        l.href = href;
+        return l;
+    };
+
+    $scope.getStats = function(url) {
+        url = url['url'];
+        var url = $scope.getLocation(url);
+        var pathArray = url.pathname.split('/');
+
+        if (url.hostname != "github.com"){
+            //TODO: tell not a github url
+            console.log("Not a github url!");
+            return;
+        }
+        $http({
+            method: "get",
+            url: "https://api.github.com/repos/" + pathArray[1] + "/" + pathArray[2] + "/languages",
+            headers: {'Accept': 'application/json',
+                      'Content-Type': "application/json"},
+        }).success(function (response) {
+            var total = 0;
+            var percentageFinal = {};
+
+            keys = Object.keys(response);
+
+            for (var i=0; i < keys.length; i++){
+                total += response[keys[i]];
+            }
+            for (var i=0; i < keys.length; i++){
+                percentageFinal[keys[i]] = response[keys[i]] * 100 / total;
+            }
+            console.log(percentageFinal)
+            return percentageFinal;
+        });
+    };
 
     $scope.addForReview = function () {
-        var _new_name = $scope.newName.trim();
-        var _new = $scope.newUrl.trim();
 
-        var obj = JSON.parse('{"name": "' + _new_name + '", "url": "' + _new + '"}');
-        console.log(_new)
-        console.log(_new_name)
+        if (!$scope.newName || !$scope.newUrl) {
+            $scope.showWarning = true;
+            console.log($scope.newName)
+            console.log($scope.newUrl)
 
-        if (!_new || !_new_name) {
             return;
         }
 
+        var _new_name = $scope.newName.trim();
+        var _new = $scope.newUrl.trim();
+
+        if (!_new || !_new_name) {
+            $scope.showWarning = true;
+            return;
+        }
+
+        if (!_new || !_new_name) {
+            $scope.showWarning = true;
+            return;
+        }
+
+        var _newUrl = $scope.getLocation(_new);
+        if (_newUrl.hostname != "github.com"){
+            console.log(_newUrl.hostname)
+            $scope.showGithubWarning = true;
+            return;
+        }
+        var obj = JSON.parse('{"name": "' + _new_name + '", "url": "' + _new + '"}');
+        
         for (var i=0; i < $scope.existing.length; i++){
             if (Object.keys($scope.existing[i])[0] == _new){
                 return;
@@ -37,7 +96,8 @@ app.controller('firstController', ['$scope', '$http', function ($scope, $http) {
         }).success(function () {
             console.log("success!");
         });
-
+        $scope.showUWarning = false;
+        $scope.showGithubWarning = false;
         $scope._new = '';
     };
 
